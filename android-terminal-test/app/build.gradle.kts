95ac80a5b58cb3c394593cafb235eb28ffe41b00
stelmashchukyurii-maker <stelmashchukyurii@gmail.com>
2026-08-21 00:19:41 +0200

    Use Android API 36 for stable CI build

--- android-terminal-test/app/build.gradle.kts
+++ android-terminal-test/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.florivo.terminaltest"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.florivo.terminaltest"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1-ui-local-test"
     }
